79ab059a179ffc705e07c5c3b07a17e11e4feb15
Miguecapi <zamiguelangel.zam@gmail.com>
2025-05-30 10:04:55 -0500

    json commit

--- main.py
+++ main.py
@@ -18,29 +18,29 @@ async def enviar_calculos():
     json_recomendaciones = {
         "date": datetime.utcnow().isoformat(),
         "energy": {
-            "applianceHours": 24,
-            "lightBulbs": 20,
-            "gasTanks": 5,
-            "hvacHours": 24
+            "applianceHours": 1,
+            "gasTanks": 3,
+            "hvacHours": 4,
+            "lightBulbs": 1
         },
         "food": {
-            "redMeat": 14,
-            "whiteMeat": 14,
-            "dairy": 21,
-            "vegetarian": 21
+            "dairy": 5,
+            "redMeat": 1,
+            "vegetarian": 0,
+            "whiteMeat": 1
         },
         "transport": {
-            "carKm": 1000,
-            "publicKm": 500,
-            "domesticFlights": 20,
-            "internationalFlights": 10
+            "carKm": 1,
+            "domesticFlights": 1,
+            "internationalFlights": 0,
+            "publicKm": 1
         },
         "waste": {
-            "trashBags": 20,
-            "foodWaste": 20,
-            "plasticBottles": 50,
-            "paperPackages": 20
+            "foodWaste": 5,
+            "paperPackages": 1,
+            "plasticBottles": 1,
+            "trashBags": 1
         },
-        "total": 50.0
+        "result": 66.3
     }
     return {"response_data": json_recomendaciones}
